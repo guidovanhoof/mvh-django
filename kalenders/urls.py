fb1fc6ffa4d34cd319d888e80dc688d18fb13780
Guido Van Hoof <guido.van.hoof3@telenet.be>
2022-10-13 19:57:06 +0200

    Kalenders overzicht/detail template aangemaakt

--- kalenders/urls.py
+++ kalenders/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.kalenders, name="kalenders"),
+    path('<jaar>', views.kalender, name="kalender"),
 ]
--- kalenders/views.py
+++ kalenders/views.py
@@ -2,4 +2,17 @@ from django.shortcuts import render
 
 
 def kalenders(request):
-    return render(request, "kalenders/kalenders.html")
+    kalenders_list = [
+        {"jaar": 2020},
+        {"jaar": 2021},
+        {"jaar": 2022},
+    ]
+    return render(
+        request, "kalenders/kalenders.html", {"kalenders": kalenders_list}
+    )
+
+
+def kalender(request, jaar):
+    return render(
+        request, "kalenders/kalender.html", {"kalender": {"jaar": jaar}}
+    )
